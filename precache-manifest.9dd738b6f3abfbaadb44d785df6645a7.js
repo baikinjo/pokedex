7cece3f624e26a69edf77d0674379aff18fd2a3a
Injo Baik <baikinjo.28@gmail.com>
2019-08-31 02:35:52 -0700

    Updates

---
+++ precache-manifest.9dd738b6f3abfbaadb44d785df6645a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80227fe107827c3a76748d62cb091486",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d9fc7f64c0edaab3be7d",
+    "url": "/pokedex/static/css/main.1fbcfb20.chunk.css"
+  },
+  {
+    "revision": "ca92cd0d12355ae16668",
+    "url": "/pokedex/static/js/2.c9a5f18c.chunk.js"
+  },
+  {
+    "revision": "d9fc7f64c0edaab3be7d",
+    "url": "/pokedex/static/js/main.7cafdff6.chunk.js"
+  },
+  {
+    "revision": "15bab4aeb5ff93176387",
+    "url": "/pokedex/static/js/runtime~main.fa927385.js"
+  }
+]);
